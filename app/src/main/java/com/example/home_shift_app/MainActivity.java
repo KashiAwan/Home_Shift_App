8f5c712b24337d15e503e349d147045a78f838d4
KASHIF IJAZ <fariawan7654@gmail.com>
2020-01-17 09:27:02 +0500

    first

--- app/src/main/java/com/example/home_shift_app/MainActivity.java
+++ app/src/main/java/com/example/home_shift_app/MainActivity.java
@@ -1,26 +1,36 @@
 package com.example.home_shift_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText emaill,pass;
+    EditText log_emaill, log_pass;
     Button loginbut;
     TextView createone;
+    DatabaseReference refff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        emaill = findViewById(R.id.Email);
-        pass = findViewById(R.id.Passwrd);
+        log_emaill = findViewById(R.id.Email);
+        log_pass = findViewById(R.id.Passwrd);
         loginbut = findViewById(R.id.Loginbut);
         createone = findViewById(R.id.createone);
 
@@ -29,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                refffunction();
             }
         });
 
@@ -36,7 +47,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(MainActivity.this, Signupclass.class);
+                startActivity(intent);
             }
         });
+    }
+
+    public void refffunction() {
+
+        refff = FirebaseDatabase.getInstance().getReference().child("We Shift");
+
+        refff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String Email1 = dataSnapshot.child("Userss").getValue().toString();
+                String Password1 = dataSnapshot.child("Userss").child("Email").getValue().toString();
+
+                if (log_emaill.getText().toString().equals(Email1) && log_pass.getText().toString().equals(Password1)) {
+                    minlocfun();
+                } else if (!log_emaill.getText().toString().equals(Email1) && log_pass.getText().toString().equals(Password1)) {
+                    Toast.makeText(MainActivity.this, "Enter Valid Email", Toast.LENGTH_SHORT).show();
+                } else if (log_emaill.getText().toString().equals(Email1) && !log_pass.getText().toString().equals(Password1)) {
+                    Toast.makeText(MainActivity.this, "Enter Valid Password", Toast.LENGTH_SHORT).show();
+                } else if (!log_emaill.getText().toString().equals(Email1) && !log_pass.getText().toString().equals(Password1)) {
+                    Toast.makeText(MainActivity.this, "Sign Up First or Enter Email & Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    public void minlocfun(){
+        Intent intent = new Intent(this, UserDashboard.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/home_shift_app/Signupclass.java
+++ app/src/main/java/com/example/home_shift_app/Signupclass.java
@@ -3,12 +3,40 @@ package com.example.home_shift_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signupclass extends AppCompatActivity {
+
+    EditText namee, emailll,passs;
+    Button signupnbut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signupclass);
+
+        namee = findViewById(R.id.signNamee);
+        emailll = findViewById(R.id.signEmail);
+        passs = findViewById(R.id.signpasswrd);
+        signupnbut = findViewById(R.id.signupbut);
+
+
+        signupnbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database=FirebaseDatabase.getInstance();
+                DatabaseReference myRef= database.getReference("We Shift");
+
+                myRef.child("Userss").child("Email").setValue(emailll.getText().toString());
+                myRef.child("Userss").child(emailll.getText().toString()).child("Name").setValue(passs.getText().toString());
+                myRef.child("Userss").child(emailll.getText().toString()).child("Password").setValue(passs.getText().toString());
+            }
+        });
+
     }
 }
