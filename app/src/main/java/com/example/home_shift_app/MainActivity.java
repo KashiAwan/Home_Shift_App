75a1702635d555503dcf9567429a96cbaa2005a8
KASHIF IJAZ <fariawan7654@gmail.com>
2020-01-17 08:24:30 +0500

    first

--- app/src/main/java/com/example/home_shift_app/MainActivity.java
+++ app/src/main/java/com/example/home_shift_app/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.home_shift_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emaill,pass;
+    Button loginbut;
+    TextView createone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emaill = findViewById(R.id.Email);
+        pass = findViewById(R.id.Passwrd);
+        loginbut = findViewById(R.id.Loginbut);
+        createone = findViewById(R.id.createone);
+
+
+        loginbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        createone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
