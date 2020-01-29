3e95373ee26ef0cba510f89a3221c1255a2e33af
KASHIF IJAZ <fariawan7654@gmail.com>
2020-01-29 13:18:50 +0500

    first

--- app/src/main/java/com/example/home_shift_app/MainActivity.java
+++ app/src/main/java/com/example/home_shift_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     Button loginbut;
     TextView createone;
     DatabaseReference refff;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +81,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        sharedPreferences = getSharedPreferences("saveemail", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("saveemaail",log_emaill.getText().toString());
+        editor.apply();
     }
 
     public void minlocfun(){
--- app/src/main/java/com/example/home_shift_app/UserDashboard.java
+++ app/src/main/java/com/example/home_shift_app/UserDashboard.java
@@ -2,13 +2,32 @@ package com.example.home_shift_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserDashboard extends AppCompatActivity {
+
+    TextView pickuploc, dropofloc;
+
+
+
+
+    //String signinemaill;
+    //SharedPreferences sharedPreferences = getSharedPreferences("saveemail", MODE_PRIVATE);
+    //signinemaill = sharedPreferences.getString("saveemaail", String.valueOf(MODE_PRIVATE));
+    //signnamee.setText(signinemaill);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        pickuploc = findViewById(R.id.pickuploc);
+        dropofloc = findViewById(R.id.drpofloc);
+
+
+
     }
 }
